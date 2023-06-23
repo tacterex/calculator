7fd95b146201820051067487a7c38ac4622e3f04
tacterex <ihor070722@gmail.com>
2023-06-23 13:32:30 +0300

    Chenged animation

--- js/markup.js
+++ js/markup.js
@@ -11,6 +11,30 @@ const BLACK_COLOR = getComputedStyle(root).getPropertyValue('--black-color')
 const WHITE_SHADOW = 'rgba(255, 255, 255, 0.16)'
 const BLACK_SHADOW = 'rgba(0, 0, 0, 0.16)'
 
+const ANIMATION_DURATION = {
+    duration: 800,
+    iterations: 1
+}
+
+const ANIMATIONS = {
+    BLACK: {
+        ADVANCED: {
+            transform: 'translateX(0)'
+        },
+        BASIC: {
+            transform: 'translateX(-100vw)'
+        }
+    },
+    WHITE: {
+        ADVANCED: {
+            transform: 'translateX(100vw)'
+        },
+        BASIC: {
+            transform: 'translateX(0)'
+        }
+    }
+}
+
 let darkTheme = false;
 
 const updateScreen = (event) => {
@@ -24,9 +48,10 @@ const setDarkTheme = () => {
     root.style.setProperty('--white-color', BLACK_COLOR)
     root.style.setProperty('--shadow', WHITE_SHADOW)
     let distance = window.innerWidth * 0.9 - window.innerHeight * 0.08
+    console.log(distance)
     switcher.children[0].style.transform = 'translateX(' + String(distance) + 'px)'
-    basic_table.style.transform = 'translateX(-100vw)'
-    advanced_table.style.transform = 'translateX(0)'
+    basic_table.className = "black_basic"
+    advanced_table.className = "black_advanced"
 }
 
 const setLightTheme = () => {
@@ -34,8 +59,8 @@ const setLightTheme = () => {
     root.style.setProperty('--white-color', WHITE_COLOR)
     root.style.setProperty('--shadow', BLACK_SHADOW)
     switcher.children[0].style.transform = 'translate(0, 0)'
-    basic_table.style.transform = 'translateX(0)'
-    advanced_table.style.transform = 'translateX(100vw)'
+    basic_table.className = "white_basic"
+    advanced_table.className = "white_advanced"
 }
 
 setLightTheme()
